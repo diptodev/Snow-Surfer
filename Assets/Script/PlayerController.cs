b03fe637bec47106cc291c089fe4f859ef66ab72
diptodev <127576890+diptodev@users.noreply.github.com>
2026-07-23 13:38:08 +0600

    speed booster added

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,27 +2,51 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 { 
-    InputAction moveAction;
     [SerializeField] float torqueAmount=10f;
+    [SerializeField] float baseSpeed=15f;
+    [SerializeField] float boostSpeed=20f;
+    SurfaceEffector2D surfaceEffector2D;
+    InputAction moveAction;
+    
+ int boosterLayerIndex=LayerMask.NameToLayer("Booster");
+ Vector2 moveVector;
     Rigidbody2D myRigidBody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction=InputSystem.actions.FindAction("Move");
         myRigidBody=GetComponent<Rigidbody2D>();
+        surfaceEffector2D=FindAnyObjectByType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-    Vector2 moveVector;
-    moveVector=moveAction.ReadValue<Vector2>();
+        moveVector=moveAction.ReadValue<Vector2>();
+   rotatePlayer();
+   boostPlayer();
+    }
+    void rotatePlayer()
+    {
+         
+    
     if (moveVector.x>0)
     {
         myRigidBody.AddTorque(-torqueAmount);
     }else if (moveVector.x<0)
         {
             myRigidBody.AddTorque(torqueAmount);
+        }
+    }
+    void boostPlayer()
+    {
+        if (moveVector.y>0)
+        {
+        //TODO
+        surfaceEffector2D.speed=boostSpeed;
+        }else
+        {
+            surfaceEffector2D.speed=baseSpeed;
         }
     }
 }
