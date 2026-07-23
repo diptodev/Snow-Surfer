bfbfcf22d9d2aa5f2ca4ce56624b69d7c3753826
diptodev <127576890+diptodev@users.noreply.github.com>
2026-07-23 12:52:55 +0600

    level added

--- Assets/Script/CrashDetector.cs
+++ Assets/Script/CrashDetector.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashParticle;
+    [SerializeField] ParticleSystem playerParticle;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerFloorIndex=LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer==layerFloorIndex)
         {
-            SceneManager.LoadScene(0);
+            playerParticle.Stop();
+            crashParticle.Play();
+            Invoke("reloadScene",1f);
         }
+    }
+    void reloadScene()
+    {
+        crashParticle.Stop();
+        SceneManager.LoadScene(0);
     }
 }
